b743df9374ebd48b4107c58254010b8c641fb2e0
Dcode36 <kadamdigvijay36@gmail.com>
2024-10-01 02:35:50 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle navigation menu on mobile view
+function toggleNav() {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+}
+
+AOS.init();
